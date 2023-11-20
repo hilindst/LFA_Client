468f798a9c7e33d35dd2ac960522b5ae6fa26556
hilindst <114443718+hilindst@users.noreply.github.com>
2023-11-20 16:57:42 -0600

    fixing up kanban-board and auth css

--- TaskIt-Project/planning.md
+++ TaskIt-Project/planning.md
@@ -1,9 +1,12 @@
-#TaskIt
+# Task It
 
-sidebar 
-  task list link
-  logo
-  user info
-
-  google icons
-  export as svg
+1) Auth Issues
+   1) Task-list & Routing issues
+   2) object subscription issues
+   3) Set up testing login pop-up
+2) Create Kan-ban Cards
+3) Data-Binding Tasks
+   1) add task
+   2) delete task
+   3) edit task
+   4) view task
--- TaskIt-Project/taskit-project/src/app/dashboard/kanban-board/kanban-board.component.ts
+++ TaskIt-Project/taskit-project/src/app/dashboard/kanban-board/kanban-board.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TaskserviceService } from 'src/app/shared/services/taskservice.service';
 
 @Component({
   selector: 'app-kanban-board',
   templateUrl: './kanban-board.component.html',
   styleUrls: ['./kanban-board.component.css']
 })
-export class KanbanBoardComponent {
+export class KanbanBoardComponent implements OnInit{
+  tasks = this.taskService.tasks;
+  constructor (private taskService, TaskserviceService, private route: ActivatedRoute ) {}
+
+  ngOnInit(): void {
+
+  }
 
 }
