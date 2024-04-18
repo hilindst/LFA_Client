c62745011c20ee7d10c0dc07c9240e3810c5cc0e
hilindst <114443718+hilindst@users.noreply.github.com>
2024-04-18 01:51:24 -0500

    working on prod

--- lfa_Client/src/app/footer/footer.component.ts
+++ lfa_Client/src/app/footer/footer.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-// import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 
 @Component({
@@ -11,37 +9,4 @@ import { NgForm } from '@angular/forms';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-  isModalShown: boolean = false;
-
-  /* sendEmail(contactForm: NgForm) {
-    if (contactForm.valid) {
-      const templateParams = {
-        user_name: 'NPC character Archive Team',
-        email_subject: contactForm.value.subject,
-        email_message: contactForm.value.message,
-        sender_email: contactForm.value.email,
-      };
-      // to send the email
-      // emailjs
-      //   .send(
-      //     environment.serviceID,
-      //     environment.templateID,
-      //     templateParams,
-      //     environment.userID
-      //   )
-      //   .then(
-      //     (res) => {
-      //       console.log('SUCCES', res.status, res.text);
-      //       contactForm.resetForm();
-      //     },
-      //     (err) => {
-      //       console.log('FAILED...', err);
-      //     }
-      //   );
-    }
-  }
-
-  closeModal() {
-    this.isModalShown = false;
-  }*/
 }
